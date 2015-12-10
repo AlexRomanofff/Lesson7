a8d16538dbab0db5878cd3d720193d1336266453
AlexRomanofff <romanov23@bigmir.net>
2015-12-10 08:37:21 +0200

    change myStack

--- lesson7/NewStack/Stack.java
+++ lesson7/NewStack/Stack.java
@@ -2,17 +2,16 @@ package lesson7.NewStack;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class Stack {
 
-//    public Object [] stackObj;
+
     private ArrayList<Object> arrayList = new ArrayList<Object>();
     private int size;
 
     public Stack () {
-//        stackObj = new Object[10];
+
         size = 0;
     }
     public int size() {
@@ -20,17 +19,7 @@ public class Stack {
     }
 
     public void push (Object object) {
-//        if (size!=0 && stackObj[stackObj.length-1] != null  ) {
-//            Object [] list = Arrays.copyOf(stackObj, stackObj.length+1);
-//            stackObj = list;
-//        }
-//        for (int i = 0; i <= size; i++) {
-//            if (stackObj[i] == null) {
-//                stackObj[i] = object;
-//                break;
-//            }
-//        }
-//        size++;
+
         arrayList.add(object);
         size++;
     }
