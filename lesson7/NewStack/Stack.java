f961c475e45fe1ccd2054f4afdd285d74aa27dd0
AlexRomanofff <romanov23@bigmir.net>
2015-12-10 08:26:08 +0200

    change stack

--- lesson7/NewStack/Stack.java
+++ lesson7/NewStack/Stack.java
@@ -1,16 +1,18 @@
 package lesson7.NewStack;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class Stack {
 
-    public Object [] stackObj;
+//    public Object [] stackObj;
+    private ArrayList<Object> arrayList = new ArrayList<Object>();
     private int size;
 
     public Stack () {
-        stackObj = new Object[10];
+//        stackObj = new Object[10];
         size = 0;
     }
     public int size() {
@@ -18,24 +20,26 @@ public class Stack {
     }
 
     public void push (Object object) {
-        if (size!=0 && stackObj[stackObj.length-1] != null  ) {
-            Object [] list = Arrays.copyOf(stackObj, stackObj.length+1);
-            stackObj = list;
-        }
-        for (int i = 0; i <= size; i++) {
-            if (stackObj[i] == null) {
-                stackObj[i] = object;
-                break;
-            }
-        }
+//        if (size!=0 && stackObj[stackObj.length-1] != null  ) {
+//            Object [] list = Arrays.copyOf(stackObj, stackObj.length+1);
+//            stackObj = list;
+//        }
+//        for (int i = 0; i <= size; i++) {
+//            if (stackObj[i] == null) {
+//                stackObj[i] = object;
+//                break;
+//            }
+//        }
+//        size++;
+        arrayList.add(object);
         size++;
     }
     public Object pop() {
         if (size==0){
             throw new EmptyStackException();
         } else {
-        Object head = stackObj[size-1];
-        stackObj[size-1]=null;
+        Object head = arrayList.get(size-1);
+        arrayList.remove(size-1);
         size--;
         return head;
         }
@@ -44,7 +48,7 @@ public class Stack {
         if (size==0){
             throw new EmptyStackException();
         } else {
-        Object head = stackObj[size-1];
+        Object head = arrayList.get(size-1);
         return head;
         }
     }
