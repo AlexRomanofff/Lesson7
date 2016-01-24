9f0e08c770767de87d1d9462e34d55a5fbca1670
AlexRomanofff <romanov23@bigmir.net>
2016-01-24 09:07:19 +0200

    add Annotation for method

--- Lesson10/Containers/ApplicationManager.java
+++ Lesson10/Containers/ApplicationManager.java
@@ -1,17 +1,27 @@
 package Lesson10.Containers;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class ApplicationManager {
 
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException{
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException{
         ApplicationManager am = new ApplicationManager();
         am.runService(FirstService.class);
         am.runService(Integer.class);
     }
 
-    public void runService (Class aClass) throws InstantiationException, IllegalAccessException {
+    public <T> void runService (Class <T> aClass) throws InstantiationException, IllegalAccessException,InvocationTargetException {
 
         if (aClass.isAnnotationPresent(RunService.class)) {
-            System.out.println("Run all methods class: "+ aClass.getSimpleName());
+           System.out.println("Create new instance class: "+ aClass.getSimpleName());
+           T newInst = aClass.newInstance();
+           for (Method m : aClass.getMethods()) {
+               if (m.isAnnotationPresent(InitService.class)) {
+                   m.invoke(newInst);
+               }
+           }
+
         }
     }
 }
--- Lesson10/Containers/FirstService.java
+++ Lesson10/Containers/FirstService.java
@@ -3,7 +3,11 @@ package Lesson10.Containers;
 @RunService
 public class FirstService {
 
-
+    @InitService
     public void washing () {
+        System.out.println("Wash dishes");
+    }
+    public void cleaning () {
+        System.out.println("Clean house");
     }
 }
