0d3b91f49bb3ccc67fc9dd932ec3d16c25e852d2
AlexRomanofff <romanov23@bigmir.net>
2016-01-25 11:02:33 +0200

    Add getService

--- Lesson10/Containers/ApplicationManager.java
+++ Lesson10/Containers/ApplicationManager.java
@@ -3,25 +3,36 @@ package Lesson10.Containers;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class ApplicationManager {
+
+public class ApplicationManager <T> {
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException{
         ApplicationManager am = new ApplicationManager();
-        am.runService(FirstService.class);
-        am.runService(Integer.class);
+//        am.runService(FirstService.class);
+//        am.runService(Integer.class);
+        am.getService(FirstService.class);
+        am.getService(Integer.class);
     }
 
     public <T> void runService (Class <T> aClass) throws InstantiationException, IllegalAccessException,InvocationTargetException {
 
         if (aClass.isAnnotationPresent(RunService.class)) {
-           System.out.println("Create new instance class: "+ aClass.getSimpleName());
-           T newInst = aClass.newInstance();
-           for (Method m : aClass.getMethods()) {
-               if (m.isAnnotationPresent(InitService.class)) {
-                   m.invoke(newInst);
-               }
-           }
-
+            System.out.println("Create new instance class: " + aClass.getSimpleName());
+            T newInst = aClass.newInstance();
+            for (Method m : aClass.getMethods()) {
+                if (m.isAnnotationPresent(InitService.class)) {
+                    m.invoke(newInst);
+                }
+            }
+        }
+    }
+    public T getService (Class<T> aClass) throws IllegalAccessException, InstantiationException {
+        if (aClass.isAnnotationPresent(RunService.class)) {
+            System.out.println("Create new instance class: " + aClass.getSimpleName());
+            return aClass.newInstance();
+        } else {
+            System.out.println(aClass.getSimpleName()+" isn't a Service");
+            return null;
         }
     }
 }
--- Lesson10/Containers/Service.java
+++
@@ -1,5 +0,0 @@
-package Lesson10.Containers;
-
-public interface Service {
-
-}
