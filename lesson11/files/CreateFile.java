c663eb1f9f1b5931c6523b52e00feaa792015235
AlexRomanofff <romanov23@bigmir.net>
2016-02-02 15:47:01 +0200

    create file

---
+++ lesson11/files/CreateFile.java
@@ -0,0 +1,17 @@
+package lesson11.files;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) throws IOException{
+
+        String path = "D:"+File.separator+"new_Directory"+File.separator+"newFile.txt";
+
+        File f = new File(path);
+        f.getParentFile().mkdirs();
+        f.createNewFile();
+
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
