13dc0bfc610250499d2ba2735996e014a1cb55f9
AlexRomanofff <romanov23@bigmir.net>
2016-02-03 12:27:16 +0200

    copy file with data by buffer

--- lesson11/frame3/CopyFiles.java
+++ lesson11/frame3/CopyFiles.java
@@ -9,20 +9,20 @@ public class CopyFiles {
     public void copyFile (File file) throws IOException {
 
         FileInputStream reader = new FileInputStream(file);
-        String path = file.getAbsolutePath();
+        BufferedInputStream bif = new BufferedInputStream(reader, 256);
 
+        String path = file.getAbsolutePath();
         String newPath=path.substring(0,path.indexOf("."))+"Copy"+path.substring(path.indexOf("."));
 
         File f = new File(newPath);
         f.createNewFile();
 
-        FileWriter fr  = new FileWriter(newPath,true);
+        FileWriter fr  = new FileWriter(newPath);
+        BufferedWriter br = new BufferedWriter(fr,256);
         StringBuilder builder = new StringBuilder();
-        BufferedWriter br = new BufferedWriter(fr);
 
-        int i;
-        while ((i=reader.read())!=-1){
-           builder.append((char)i);
+        while (bif.available()>0) {
+           builder.append((char)bif.read());
         }
         String data = builder.toString();
 
--- lesson11/frame3/Demo.java
+++ lesson11/frame3/Demo.java
@@ -1,5 +1,6 @@
 package lesson11.frame3;
 
+
 import java.io.File;
 import java.io.IOException;
 
@@ -7,8 +8,8 @@ import java.io.IOException;
 public class Demo {
 
     public static void main(String[] args) throws IOException {
-        CopyFiles cf = new CopyFiles();
 
+        CopyFiles cf = new CopyFiles();
         File file = new File("D:"+File.separator+"new_Directory"+File.separator+"newFile.txt");
         cf.copyFile(file);
     }
