cd369c8ccf36d47006dea105e8a3e84870b600da
AlexRomanofff <romanov23@bigmir.net>
2016-02-04 11:35:49 +0200

    change sysout

---
+++ lesson11/symbolsStrings/ChangeSysout.java
@@ -0,0 +1,15 @@
+package lesson11.symbolsStrings;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class ChangeSysout {
+
+    public static void main(String[] args) throws Exception{
+        System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("D:"+ File.separator+"new_Directory"+File.separator+"newFile.txt")),true));
+        System.out.println("hello my friend");
+        System.out.println("my name is Alex");
+    }
+}
