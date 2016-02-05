f0e9cd13315502989a93e3374499f6877533d2e3
AlexRomanofff <romanov23@bigmir.net>
2016-02-05 12:30:38 +0200

    copy file with data

--- lesson11/Demo.java
+++ lesson11/Demo.java
@@ -2,10 +2,11 @@ package lesson11;
 
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 public class Demo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         byte [] bytes = {4,15,77,22};
         ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
         new ByteLearning().printStreamData(stream);
